0fc8452da07d4c98037467e83c15b9ef220a27a2
aleksey <afilatov2002@gmail.com>
2020-05-25 14:07:56 +0300

    Updates

---
+++ precache-manifest.fa4f5befbc356d38f5c5e5256397e4a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a4c8a3298e0a991f2056cb7603a09e",
+    "url": "/final-project/index.html"
+  },
+  {
+    "revision": "a774210329e79de2ded5",
+    "url": "/final-project/static/css/main.d925eff8.chunk.css"
+  },
+  {
+    "revision": "6e300097bc1a971c5f48",
+    "url": "/final-project/static/js/2.a4105e4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/final-project/static/js/2.a4105e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a774210329e79de2ded5",
+    "url": "/final-project/static/js/main.22dd0dae.chunk.js"
+  },
+  {
+    "revision": "bc02de704b2f481ff4cc",
+    "url": "/final-project/static/js/runtime-main.89bd77b1.js"
+  }
+]);
